c0d39bdaa6058fad4c2cb5d9b6b9f927f41355a8
olixu <273601727@qq.com>
2021-06-27 12:00:36 +0800

    modify the usage of the argparse

--- downloader.py
+++ downloader.py
@@ -1,30 +1,52 @@
-# -*- coding:utf-8 -*-
+# -*- encoding: utf-8 -*-
+'''
+@File    :   downloader.py
+@Time    :   2021/06/27 10:24:10
+@Author  :   olixu 
+@Version :   1.0
+@Contact :   273601727@qq.com
+@WebSite    :   https://blog.oliverxu.cn
+'''
 
-import requests
-import json
-import shutil
+# here put the import lib
 import os
-import time
 import sys
-from lxml import etree
-from bs4 import BeautifulSoup
-from collections import defaultdict
-import fitz
 import time
 import argparse
 import random
+import json
+import shutil
+from collections import defaultdict
+from urllib.parse import quote
+import requests
+from lxml import etree
+import fitz
+import pdb
+
+# To DO list
+# 1. 可以抓取元数据信息
+# 2. 进行选择抓取多少个page的pdf数据
+# 3. 
 
 def main():
+    """
+    下载学位论文入口程序：
+
+    调用方式：python downloader.py --pages '1-2' --major '计算机'
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pages', type=str, required=True)
+    parser.add_argument('--content', type=str, required=True)    #   
+    parser.add_argument('--choose_key', type=str, required=True) # 1.主题(topic) 2.题名() 3.关键词() 4.作者() 5.院系 6.(专业) 7.(导师) 8.(年份)
+    parser.add_argument('--xuewei', type=str, required=True) # 0：硕博论文 1：博士论文 2：硕士论文
+    parser.add_argument('--px', type=str, required=True) # 1：按题名字顺序排序 2：按学位年度倒排序
+    parser.add_argument('--page', type=str, required=True)  # 希望抓取多少页的数据，一页的元数据是20篇论文
     args = parser.parse_args()
-    pages = args.pages.split('-')
-    #print(pages[0])
-    #print(pages[1])
-    info_url = "http://thesis.lib.sjtu.edu.cn/sub.asp?content=%E8%87%AA%E5%8A%A8%E5%8C%96&choose_key=department&xuewei=1&px=2&page="
-    #info_url = "http://thesis.lib.sjtu.edu.cn/sub.asp?content=%E8%AE%A1%E7%AE%97%E6%9C%BA&choose_key=department&xuewei=1&px=2&page="
+
+    info_url = "http://thesis.lib.sjtu.edu.cn/sub.asp?content={}&choose_key={}&xuewei={}&px={}&page=".format(quote(args.content), args.choose_key, args.xuewei, args.px)
+    pages = args.page.split('-')
     pages = [int(pages[0]), int(pages[1])]
     papers = download_main_info(info_url, pages)
+    pdb.set_trace()
     jpg_dir = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) + "".join(random.sample('zyxwvutsrqponmlkjihgfedcba23429837498234',5))
     for paper in papers:
         print(100*'@')
@@ -74,16 +96,9 @@ def download_main_info(info_url: str, pages: list):
     for page in range(pages[0], pages[1]+1):
         print("正在抓取第{}页的info".format(page))
         info_url_construction = info_url + str(page)
-        #print(info_url_construction)
-        #print(50*'&')
         response = result.get(info_url_construction, headers=headers, allow_redirects=False)
-        #with open('test.html', 'wb') as f:
-        #    f.write(response.content)
-
         html = etree.HTML(response.content, etree.HTMLParser())
-
         for i in range(2, 22):
-            #print(i)
             # 有些是论文保密，所以link需要错误处理
             info_dict = defaultdict(str)
             try:
@@ -92,29 +107,15 @@ def download_main_info(info_url: str, pages: list):
                 mentor = html.xpath('/html/body/section/div/div[3]/div[2]/table/tr[{}]/td[6]/div/text()'.format(i))[0]
                 year = html.xpath('/html/body/section/div/div[3]/div[2]/table/tr[{}]/td[8]/div/text()'.format(i))[0]
                 link = "http://thesis.lib.sjtu.edu.cn/" + html.xpath('/html/body/section/div/div[3]/div[2]/table/tr[{}]/td[9]/div/a[2]/@href'.format(i))[0]
-                #print(filename)
-                #print(author)
-                #print(mentor)
-                #print(year)
-                #print(link)
-                #print(50*'#')
                 info_dict['filename'] = filename
                 info_dict['author'] = author
                 info_dict['mentor'] = mentor
                 info_dict['year'] = year
                 info_dict['link'] = link
                 papers.append(info_dict)
-            except:
-                pass
-   # print(papers)
+            except Exception as e:
+                print(e)
     print("总共抓取到{}个元数据信息".format(len(papers)))
- 
-
-    #soup = BeautifulSoup(response.text.encode('utf-8'), 'html.parser')
-    #table = soup.find('table')
-    #print(table)
-
-
     return papers
 
 def download_jpg(url: str, jpg_dir: str):
